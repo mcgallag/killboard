80edc24865e9315996436bd0bab669bd2a4bf5af
Mike Gallagher <mcgallag@gmail.com>
2018-10-22 20:06:23 -0400

    initial commit of lua script for CE

---
+++ cheat_engine_script.lua
@@ -0,0 +1,134 @@
+setup = function()
+  addrList = nil
+  addrList = getAddressList()
+
+  currentKills = getValue("Player's Kills (In Mission)")
+  boardKills = getValue("Board Kills (Player)")
+  sorties = getValue("Board Sorties (Player)")
+  rank = getValue("Board Rank (Player)")
+  callsign = getValue("Callsign (Player)")
+  name = getValue("Name (Player)")
+
+  changedValue = false
+
+  outputFormat = "{\n" ..
+                 "  \"currentKills\": %d,\n" ..
+                 "  \"boardKills\": %d,\n" ..
+                 "  \"sorties\": %d,\n" ..
+                 "  \"rank\": %d,\n" ..
+                 "  \"callsign\": \"%s\",\n" ..
+                 "  \"name\": \"%s\"\n" ..
+                 "}\n"
+end
+
+printDebug = function()
+  print(string.format(outputFormat, currentKills, boardKills, sorties, rank, callsign, name))
+end
+
+getValue = function(desc)
+  local mr = addrList.getMemoryRecordByDescription(desc)
+  return mr.Value
+end
+
+writeJson = function()
+  currentKillsOutput = "  \"currentKills\": " .. currentKills .. ",\n"
+  boardKillsOutput = "  \"boardKills\": " .. boardKills .. ",\n"
+  sortiesOutput = "  \"sorties\": " .. sorties .. ",\n"
+  rankOutput = "  \"rank\": " .. rank .. ",\n"
+  callsignOutput = "  \"callsign\": \"" .. callsign .. "\",\n"
+  nameOutput = "  \"name\": \"" .. name .. "\"\n"
+
+  local fpJson = io.open("H:\\Prog\\killboard\\fromgame.json", "w+")
+
+  if (fpJson) then
+    fpJson:write("{\n")
+      fpJson:write(currentKillsOutput)
+      fpJson:write(boardKillsOutput)
+      fpJson:write(sortiesOutput)
+      fpJson:write(rankOutput)
+      fpJson:write(callsignOutput)
+      fpJson:write(nameOutput)
+    fpJson:write("}\n")
+    fpJson:close()
+    print("fromgame.json written")
+  else
+    print("Failure")
+    print(fpJson)
+  end
+
+  fileWritten = true
+end
+
+cfcDebug = function()
+  print("")
+  print("")
+  print("DEBUG")
+  if (prevCurrentKills ~= currentKills) then
+    print(string.format("CurrentKills: %x => %x", prevCurrentKills, currentKills))
+    print(prevCurrentKills)
+    print(currentKills)
+  end
+  if (prevBoardKills ~= boardKills) then
+    print(string.format("BoardKills: %d => %d", prevBoardKills, boardKills))
+  end
+  if (prevSorties ~= sorties) then
+    print(string.format("Sorties: %d => %d", prevSorties, sorties))
+  end
+  if (prevRank ~= rank) then
+    print(string.format("Rank: %d => %d", prevRank, crank))
+  end
+  if (prevCallsign ~= callsign) then
+    print(string.format("Callsign: %d => %d", prevCallsign, callsign))
+  end
+  if (prevName ~= name) then
+    print(string.format("Name: %d => %d", prevName, name))
+  end
+end
+
+checkForChanges = function()
+  changedValue = ((prevCurrentKills ~= currentKills) or
+                  (prevBoardKills ~= boardKills) or
+                  (prevSorties ~= sorties) or
+                  (prevRank ~= rank) or
+                  (prevCallsign ~= callsign) or
+                  (prevName ~= name))
+  if (prevBoardKills ~= boardKills) then
+    print("Exception")
+    local mr = addrList.getMemoryRecordByDescription("Player's Kills (In Mission)")
+    local addr = mr.getCurrentAddress()
+    currentKills = 0
+    prevCurrentKills = 0
+    writeSmallInteger(addr, currentKills)
+  end
+end
+
+outputMain = function(timer)
+  prevCurrentKills = currentKills
+  prevBoardKills = boardKills
+  prevSorties = sorties
+  prevRank = rank
+  prevCallsign = callsign
+  prevName = name
+
+  addrList = getAddressList()
+
+  currentKills = getValue("Player's Kills (In Mission)")
+  boardKills = getValue("Board Kills (Player)")
+  sorties = getValue("Board Sorties (Player)")
+  rank = getValue("Board Rank (Player)")
+  callsign = getValue("Callsign (Player)")
+  name = getValue("Name (Player)")
+
+  checkForChanges()
+
+  if (changedValue == true) then
+    cfcDebug()
+    writeJson()
+  end
+end
+
+setup()
+
+local outputTimer = createTimer(getLuaEngine())
+outputTimer.Interval = 1000
+outputTimer.OnTimer = outputMain
